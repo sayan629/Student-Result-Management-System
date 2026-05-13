068c5ed06f3782666edfa79e57a1a6d708e83a84
sayan629 <sayanpal771@gmail.com>
2026-05-14 01:59:11 +0530

    Student Section done..

--- result.py
+++ result.py
@@ -22,8 +22,9 @@ class resultClass:
         self.var_course = StringVar()  
         self.var_marks_ob = StringVar()
         self.var_total_marks = StringVar()
-        
+        self.var_percentage = StringVar()
         self.roll_list = []
+        self.fetch_roll()
         
         lbl_select = Label(self.root, text="Select Student", font=("goudy old style", 20, "bold"),bg="white").place(x=50, y=100)
         lbl_name = Label(self.root, text="Name", font=("goudy old style", 20, "bold"),bg="white").place(x=50, y=160)
@@ -35,7 +36,7 @@ class resultClass:
         self.txt_student.place(x=280, y=100, width=200)
         self.txt_student.set("Select")
         
-        btn_search=Button(self.root, text="Search", font=("goudy old style", 15, "bold"), bg="#03a9f4", fg="white", cursor="hand2")
+        btn_search=Button(self.root, text="Search", font=("goudy old style", 15, "bold"), bg="#03a9f4", fg="white", cursor="hand2",command=self.search)
         btn_search.place(x=500, y=100, width=100, height=28)
         
         txt_name = Entry(self.root, textvariable=self.var_name, font=("goudy old style", 20, "bold"), bg="lightyellow",state='readonly').place(x=280, y=160, width=320)
@@ -44,8 +45,8 @@ class resultClass:
         txt_total_marks = Entry(self.root, textvariable=self.var_total_marks, font=("goudy old style", 20, "bold"), bg="lightyellow").place(x=280, y=340, width=320)
 
         # --- Buttons ---
-        btn_add = Button(self.root, text="Submit", font=("times new roman", 15, ), bg="lightgreen", activebackground="lightgreen", cursor="hand2").place(x=300, y=420, width=120, height=35)
-        btn_add = Button(self.root, text="Clear", font=("times new roman", 15, ), bg="lightgray", activebackground="lightgray", cursor="hand2").place(x=430, y=420, width=120, height=35)
+        btn_add = Button(self.root, text="Submit", font=("times new roman", 15, ), bg="lightgreen", activebackground="lightgreen", cursor="hand2",command=self.add).place(x=300, y=420, width=120, height=35)
+        btn_clear = Button(self.root, text="Clear", font=("times new roman", 15, ), bg="lightgray", activebackground="lightgray", cursor="hand2",command=self.clear).place(x=430, y=420, width=120, height=35)
 
         #---- Image ----
         self.bg_img = Image.open("images/result.png")
@@ -59,10 +60,71 @@ class resultClass:
             bg="white",
             bd=0
         )
-
         self.lbl_bg.place(x=650, y=100)
 
+
+        #---------
+    def fetch_roll(self):
+        con = sqlite3.connect(database="rms.db")
+        cur = con.cursor()
+        try:
+            cur.execute("select roll from student")
+            rows = cur.fetchall()
+            if len(rows)>0:
+                for row in rows:
+                    self.roll_list.append(row[0])    
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to {str(ex)}")
     
+    def search(self):
+        con = sqlite3.connect(database="rms.db")
+        cur = con.cursor()
+        try:
+            cur.execute("SELECT name,course FROM student WHERE roll=?",(self.var_roll.get(),))
+
+            row = cur.fetchone()
+            if row!=None:
+                self.var_name.set(row[0])
+                self.var_course.set(row[1])
+            else:
+                messagebox.showerror("Error", "No record found", parent=self.root)
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to {str(ex)}")
+
+    def add(self):
+        con = sqlite3.connect(database="rms.db")
+        cur = con.cursor()
+        try:
+            if self.var_name.get() == "":
+                messagebox.showerror("Error", "Name should be required", parent=self.root)
+            else:
+                cur.execute("select * from result where roll=? and course=?", (self.var_roll.get(), self.var_course.get()))
+                row = cur.fetchone()
+                if row != None:
+                    messagebox.showerror("Error", "Result already present, try different", parent=self.root)
+                else:
+                    percentage = (int(self.var_marks_ob.get())*100)/int(self.var_total_marks.get())
+                    self.var_percentage.set(str(percentage))
+                    cur.execute("insert into result (roll, name, course, marks_obtained, total_marks, percentage) values(?, ?, ?, ?, ?, ?)",(
+                        self.var_roll.get(),
+                        self.var_name.get(),
+                        self.var_course.get(),
+                        self.var_marks_ob.get(),
+                        self.var_total_marks.get(),
+                        str(percentage)
+                    )) 
+                    con.commit()
+                    messagebox.showinfo("Success", "Result added successfully", parent=self.root)
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to {str(ex)}")
+
+    def clear(self):
+        self.var_roll.set("Select")
+        self.var_name.set("")
+        self.var_course.set("")
+        self.var_marks_ob.set("")
+        self.var_total_marks.set("")
+
 if __name__ == "__main__":
     root = Tk()
     obj = resultClass(root)
